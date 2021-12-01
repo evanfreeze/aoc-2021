600b733be1fdabd59e2d9187d41fb05b392a6bf9
Evan Freeze <evanfreeze@gmail.com>
2021-12-01 12:27:21 -0600

    day 1 part 1

---
+++ day-01/day-01.ts
@@ -0,0 +1,16 @@
+import { readLines } from "https://deno.land/std/io/bufio.ts";
+
+const file = await Deno.open("./input.txt");
+
+let previous = null;
+let numberOfIncreases = 0;
+
+for await (const sonarReading of readLines(file)) {
+  const numericalReading = Number(sonarReading);
+  if (previous && numericalReading > previous) {
+    numberOfIncreases += 1;
+  }
+  previous = numericalReading;
+}
+
+console.info(numberOfIncreases);
