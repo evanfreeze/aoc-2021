6ecf7f6a515a0c469d4bf5c912165dc1c622ab53
Evan Freeze <evanfreeze@gmail.com>
2021-12-07 19:28:52 -0600

    day 7 part 2

--- day-07/index.ts
+++ day-07/index.ts
@@ -18,8 +18,37 @@ export function getMinFuelToAlign(input: string): number {
     );
 }
 
+export function getMinFuelToAlignHigherCost(input: string): number {
+    const numbers = input.split(",").map(Number);
+    const max = Math.max(...numbers);
+    const min = Math.min(...numbers);
+    let cursor = min;
+    let lowestFuel = Infinity;
+
+    // this feels gross but it's fairly cheap to calculate this, so why not just brute force it
+    while (cursor <= max) {
+        const fuelAtCursor = numbers.reduce((fuel, n) => getFuelForMove(n, cursor) + fuel, 0);
+        lowestFuel = Math.min(lowestFuel, fuelAtCursor);
+        cursor += 1;
+    }
+
+    return lowestFuel;
+}
+
+export function getFuelForMove(start: number, end: number): number {
+    let distance = Math.abs(start - end);
+    let total = 0;
+    while (distance > 0) {
+        total += distance;
+        distance -= 1;
+    }
+    return total;
+}
+
 function part1() {
     return getMinFuelToAlign(fileLines[0]);
 }
 
-function part2() {}
+function part2() {
+    return getMinFuelToAlignHigherCost(fileLines[0]);
+}
--- day-07/tests/index.test.ts
+++ day-07/tests/index.test.ts
@@ -1,6 +1,29 @@
-import { getMinFuelToAlign } from "..";
+import { getMinFuelToAlign, getMinFuelToAlignHigherCost, getFuelForMove } from "..";
 
 it("calculates optimal fuel alignment position", () => {
     const input = "16,1,2,0,4,2,7,1,2,14";
     expect(getMinFuelToAlign(input)).toEqual(37);
+});
+
+it("calculates the optimal fuel alignment position when farther moves cost more", () => {
+    const input = "16,1,2,0,4,2,7,1,2,14";
+    expect(getMinFuelToAlignHigherCost(input)).toEqual(168);
+});
+
+it("calculates the fuel required correctly when farther away grows fuel rate", () => {
+    const solutions = [
+        [16, 5, 66],
+        [1, 5, 10],
+        [2, 5, 6],
+        [0, 5, 15],
+        [4, 5, 1],
+        [7, 5, 3],
+        [14, 5, 45],
+        [10, 10, 0],
+        [1, -2, 6],
+        [-2, 1, 6],
+    ];
+    for (const [start, end, result] of solutions) {
+        expect(getFuelForMove(start, end)).toEqual(result);
+    }
 });
