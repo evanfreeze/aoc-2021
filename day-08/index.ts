740891aec19603f19edb78174a10115560472a95
Evan Freeze <evanfreeze@gmail.com>
2021-12-11 13:33:25 -0600

    day 8 part 2

--- day-08/index.ts
+++ day-08/index.ts
@@ -21,4 +21,120 @@ function part1() {
     return fileLines.filter(Boolean).reduce((total, line) => total + getCountForLine(line), 0);
 }
 
-function part2() {}
+export function getNumericOutputForLine(line: string) {
+    const [rawMap, rawOutput] = line.split(" | ");
+    const map = rawMap.split(" ");
+    const output = rawOutput.split(" ");
+
+    let segmentsToNumber: Record<string, number> = {};
+    let numberToSegments: string[][] = [];
+
+    while (Object.keys(segmentsToNumber).length < 10) {
+        const sortedMap = [...map].sort((a, b) => a.length - b.length);
+
+        for (const wires of sortedMap) {
+            if (wires.length === 2) {
+                segmentsToNumber[wires] = 1;
+                numberToSegments[1] = wires.split("");
+            }
+
+            if (wires.length === 3) {
+                segmentsToNumber[wires] = 7;
+                numberToSegments[7] = wires.split("");
+            }
+
+            if (wires.length === 4) {
+                segmentsToNumber[wires] = 4;
+                numberToSegments[4] = wires.split("");
+            }
+
+            if (wires.length === 7) {
+                segmentsToNumber[wires] = 8;
+                numberToSegments[8] = wires.split("");
+            }
+
+            if (wires.length === 5) {
+                const wireSegments = wires.split("");
+                // must be 2, 3, or 5
+                if (numberToSegments[1]) {
+                    if (numberToSegments[1].every((oneSeg) => wireSegments.includes(oneSeg))) {
+                        // must be 3 if it has everything 1 has
+                        segmentsToNumber[wires] = 3;
+                        numberToSegments[3] = wires.split("");
+                    } else {
+                        // could be 2 or 5
+                        if (numberToSegments[6]) {
+                            const diffsFrom6 = numberToSegments[6].filter(
+                                (segment) => !wireSegments.includes(segment),
+                            ).length;
+
+                            if (diffsFrom6 === 1) {
+                                // must be 5
+                                segmentsToNumber[wires] = 5;
+                                numberToSegments[5] = wires.split("");
+                            } else {
+                                // must be 2
+                                segmentsToNumber[wires] = 2;
+                                numberToSegments[2] = wires.split("");
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (wires.length === 6) {
+                const wireSegments = wires.split("");
+                // must be 0, 6, or 9
+                if (numberToSegments[1]) {
+                    if (numberToSegments[1].every((segment) => wireSegments.includes(segment))) {
+                        // could be be 0 or 9
+                        if (numberToSegments[8]) {
+                            if (numberToSegments[4]) {
+                                if (numberToSegments[4].every((segment) => wireSegments.includes(segment))) {
+                                    // must be 9 if it has everything 4 has
+                                    segmentsToNumber[wires] = 9;
+                                    numberToSegments[9] = wires.split("");
+                                } else {
+                                    // must be 0 if it doesn't have everything 4 has since 4 has the middle
+                                    segmentsToNumber[wires] = 0;
+                                    numberToSegments[0] = wires.split("");
+                                }
+                            }
+                        }
+                    } else {
+                        // must be 6 if it doesn't have both pieces of 1
+                        segmentsToNumber[wires] = 6;
+                        numberToSegments[6] = wires.split("");
+                    }
+                }
+            }
+        }
+    }
+
+    const result = output.reduce((result, outputNumber) => {
+        const keys = Object.keys(segmentsToNumber);
+
+        // console.log(`Looking for ${outputNumber} in map:\n\n ${JSON.stringify(segmentsToNumber, null, 2)}`);
+
+        const match = keys.find((key) => {
+            return (
+                outputNumber.length === key.length &&
+                outputNumber.split("").every((outputSeg) => key.split("").includes(outputSeg))
+            );
+        });
+
+        // console.log(`Found match! \n${match} (${segmentsToNumber[match]})`);
+
+        result.push(segmentsToNumber[match]);
+        return result;
+    }, []);
+    return Number(result.join(""));
+}
+
+export function calculatePart2Answer(lines: string[]) {
+    return lines.filter(Boolean).reduce((total, line) => total + getNumericOutputForLine(line), 0);
+}
+
+function part2() {
+    return calculatePart2Answer(fileLines);
+}
--- day-08/tests/index.test.ts
+++ day-08/tests/index.test.ts
@@ -1,4 +1,4 @@
-import { getCountForLine } from "..";
+import { calculatePart2Answer, getCountForLine, getNumericOutputForLine } from "..";
 
 it("counts number of 1, 4, 7, 8 in final output of a line", () => {
     const lines = [
@@ -15,4 +15,38 @@ it("counts number of 1, 4, 7, 8 in final output of a line", () => {
     ];
 
     lines.forEach((line) => expect(getCountForLine(line.raw)).toEqual(line.result));
+});
+
+it("gets the final numeric output of the displays", () => {
+    const lines = [
+        { raw: "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe", result: 8394 },
+        { raw: "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc", result: 9781 },
+        { raw: "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg", result: 1197 },
+        { raw: "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb", result: 9361 },
+        { raw: "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea", result: 4873 },
+        { raw: "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb", result: 8418 },
+        { raw: "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe", result: 4548 },
+        { raw: "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef", result: 1625 },
+        { raw: "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb", result: 8717 },
+        { raw: "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce", result: 4315 },
+    ];
+
+    lines.forEach((line) => expect(getNumericOutputForLine(line.raw)).toEqual(line.result));
+});
+
+it("pt2 — adds them up correctly", () => {
+    const lines = [
+        "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
+        "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc",
+        "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",
+        "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb",
+        "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea",
+        "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb",
+        "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe",
+        "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef",
+        "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb",
+        "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce",
+    ];
+
+    expect(calculatePart2Answer(lines)).toEqual(61229);
 });
